bb565a440c1fabe8b5f9be8eb644f40f47c05bd2
Gopal Kumar Das <75511425+kr395@users.noreply.github.com>
2024-02-23 08:22:14 +0000

    basic Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,50 @@
 import SearchIcon from "./SearchIcon.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard.jsx";
-
 
 // f3f5cc76     //Api Key
 const API_URL = "https://omdbapi.com?apikey=f3f5cc76";
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const searchName = async (name) => {
     const response = await fetch(`${API_URL}&s=${name}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
-    //  searchName("Spider Man")
+    searchName("Spider Man");
   }, []);
 
-  const Movie1 = {
-    "Title": "Spider-Man",
-    "Year": "2002",
-    "imdbID": "tt0145487",
-    "Type": "movie",
-    "Poster": 
-    "https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
-}
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
-        <input placeholder="Search Movies" value={"Spider Man"} onChange={()=>{}}/>
-        <img src={SearchIcon} alt="SearchIcon" onClick={()=>{}}/>
+        <input
+          placeholder="Search Movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img src={SearchIcon} alt="SearchIcon" onClick={() => {
+          searchName(searchTerm);
+        }} />
       </div>
-      <MovieCard Movie1={Movie1}/>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,22 +1,22 @@
 import { React, useState } from "react";
 
-export default function MovieCard({ Movie1 }) {
+export default function MovieCard({ movie }) {
   return (
     <div className="movie">
       <div>
-        <p>{Movie1.Year}</p>
+        <p>{movie.Year}</p>
       </div>
       <div>
         <img
           src={
-            Movie1.Poster !== "N/A" ? Movie1.Poster : "https://placehold.co/400"
+            movie.Poster !== "N/A" ? movie.Poster : "https://placehold.co/400"
           }
-          alt={Movie1.Title}
+          alt={movie.Title}
         />
       </div>
       <div>
-        <span>{Movie1.Type}</span>
-        <h3>{Movie1.Title}</h3>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
       </div>
     </div>
   );
